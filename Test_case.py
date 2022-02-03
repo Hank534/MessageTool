33617a9170eabc032da3d7a4279a7ad47b971a07
Hank534 <61291628+Hank534@users.noreply.github.com>
2022-02-03 14:50:56 -0700

    Update Test_case.py
    added some logic to check the content of the file.

--- Test_case.py
+++ Test_case.py
@@ -27,3 +27,18 @@ print(find(file_result,"C:/Users/J91307/Desktop/Projects/.ipynb_checkpoints/Mess
 print('='*80)
 
 
+"""
+Adding the logic to go read the text file verify 
+that the file contaings the correct stings written by the client. 
+Req 01: The server shall create a file in this project
+Req 02: The file shall contain some text (undecided what I want to check)
+
+"""
+expected_data = ['Thank you for connecting to the LGC repo! The socket object has been created \n', 'Thank you for connecting to the LGC repo! The socket object has been created \n']
+f = open('Report.txt', 'r')
+my_file_data = f.readlines()
+
+if expected_data == my_file_data:
+    print('this data is a match')
+else:
+    print('list does not match')
