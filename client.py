5e5c86bb981316fb9fd1d6bb80345f7505d23420
Hank534 <61291628+Hank534@users.noreply.github.com>
2022-02-08 12:12:31 -0700

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,58 @@
 # my client
-import socket 
+import socket
+from pathlib import Path
 
-c = socket.socket()
-c.connect(('localhost',9919))
-# print(c.recv(1024).decode())
-# print(c.recv(1024).decode())
+HEADER = 2048
+PORT = 9119
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SEPARATOR = '<SEPERATE>'
+FILE_FINISHED_SENDING = '<!FILE_SENT!>'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
 
-data = c.recv(1024).decode()
-data2 = c.recv(1024).decode()
-#data3 = c.recv(1024).decode()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def chunker(string: str, size: int):
+    return (string[pos:pos + size] for pos in range(0, len(string), size))
 
 
-file1 = open("Report.txt", "ab")
-file1.write(data.encode())
-file1.write(data2.encode())
-#file1.write(data3)
-file1.close()
+def send(msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
+    print(client.recv(HEADER).decode(FORMAT))
+
+
+def send_file(filepath: str):
+    with open(filepath, 'r', encoding=FORMAT) as f:
+        data = f.read()
+
+    first_bits = f'{Path(filepath).name}{SEPARATOR}'  # Easy way of getting just a file's name from its path
+    send(first_bits)  # Send the filename to the server
+    for chunk in chunker(data, HEADER-48):  # Leave a safe buffer
+        # Send each chunk of the file
+        send(f"{SEPARATOR}{chunk}")
+
+    # Tell the server that's all for this file.
+    # Now it can close the file object.
+    send(FILE_FINISHED_SENDING)
+
+
+send_file("C:/Users/J91307/Desktop/Projects/.ipynb_checkpoints/MessageTool/demo.txt")
+send(DISCONNECT_MESSAGE)
+# c = socket.socket()
+# c.connect(('localhost',9919))
+# # print(c.recv(1024).decode())
+# # print(c.recv(1024).decode())
+
+# data = c.recv(1024).decode()
+# data2 = c.recv(1024).decode()
+# #data3 = c.recv(1024).decode()
+
+
+# file1 = open("Report.txt", "ab")
+# file1.write(data.encode())
+# file1.write(data2.encode())
+# #file1.write(data3)
+# file1.close()
--- server.py
+++ server.py
@@ -1,28 +1,61 @@
 import socket
+import os.path
+import threading
 """
 This program is creating a server on a localhost 
 and will listen for clients trying to connect to it.
 The socket will have the buffer space for 5 connections.
-"""
-# takes TCP or UPD, defualt is IPV4 TCP
-soc = socket.socket()
-print('Socket Created....')
-# must be a tuple data type
-soc.bind(('localhost', 9919))
-# allocating buffer size of 5 connections 
-soc.listen(2)
-size = 2
-print("Waiting for connections....")
 
-while size > 0:
-    connections, addr = soc.accept()
-    data_connection = connections.getsockname()
-    print('The following address has been bounded to the connection via =>', addr)
-    print('='*80)
-    print('Connection made with the following socket object => ', data_connection)
-    print('='*80)
-    connections.send(bytes('Thank you for connecting to the LGC repo! ', 'utf-8'))
-    connections.send(bytes('The socket object has been created', 'utf-8'))
-    connections.close()
-    size -= 1
-    
+"""
+HEADER = 2048
+PORT = 9119
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SEPARATOR = '<SEPERATE>'
+FILE_FINISHED_SENDING = '<!FILE_SENT!>'
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f'[NEW CONNECTION] {addr} connected.')
+    connected = True
+    current_file = None
+
+    while connected:
+        data = conn.recv(HEADER).decode(FORMAT)
+        if data == DISCONNECT_MESSAGE:
+            connected = False
+        elif data == FILE_FINISHED_SENDING:
+            current_file.close()
+            current_file = None
+            conn.send(b'file received.')
+        else:
+            data = data.split(SEPARATOR)
+            if len(data) == 2 and data[1] == '':
+                # The name of the file was sent, more will follow.
+                current_file = open(data[0], 'w')
+                conn.send(b'filename recieved')
+            else:
+                # File data was send, so write it to the current file
+                current_file.write(data[1])
+                print('chunk of file recv''d')
+                conn.send(b'chunk received')
+    conn.close()
+    print(f'[DISCONNECT] {addr} disconnected')
+
+def start():
+    server.listen()
+    print(f'[LISTENING] Server is listening on {SERVER}')
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn,addr))
+        thread.start()
+        print(f'[ACTIVE CONNECTIONS] {threading.activeCount()-1}')
+
+
+print("[STARTING] server is starting...")
+start()
